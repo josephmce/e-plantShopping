ffce314aab701291643a02a160467e2bceb07fff
Joseph McEvoy <joseph_mce@live.co.uk>
2025-11-19 09:59:53 -0500

    Added ad to cart function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import addItem from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
@@ -252,6 +254,13 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); 
+        setAddedToCart((prevState) => ({
+            ...prevState, 
+            [product.name]: true,
+        }));
+    };
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -284,6 +293,7 @@ function ProductList({ onHomeClick }) {
                                         <img className="product-image" src={plant.image}/>
                                         <p>{plant.description}</p>
                                         <p className="product-price">{plant.cost}</p>
+                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                     </div>
                                 ))}
                             </div>
