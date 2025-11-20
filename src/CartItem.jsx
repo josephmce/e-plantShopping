7e3275d343c9a43aa6a03d8c40f8fcdd52f032ce
Joseph McEvoy <joseph_mce@live.co.uk>
2025-11-20 10:10:32 -0500

    Hooked up checkout alert to checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -27,7 +27,7 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart }) => {
     onContinueShopping(e);
   };
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('Thank you for your purchase!');
   };
 
 
@@ -91,7 +91,7 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
